2af1add60406e4cbab7cfa7ed38d70ede0766987
Willy Laurent Brenkman <88841329+LaurentBrenkman@users.noreply.github.com>
2022-07-13 22:42:32 +0700

    tugas tambahan

--- nomor3.js
+++ nomor3.js
@@ -1,38 +1,30 @@
-function nmr3(n) {
-    if (n%2 !== 0) {
-        for (let i = 0; i < n; i++) {
-            console.log("\n")
-            for (let j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1) {
-                    if (j == 0 || j == ((n - 1) / 2) || j == n - 1) {
-                        process.stdout.write("*" + " ");
-                    }
-                    else {
-                        process.stdout.write("#" + " ");
-                    }
-                }
-                else if (i == ((n - 1) / 2)) {
-                    if (j == ((n - 1) / 2)) {
-                        process.stdout.write("#" + " ");
-                    }
-                    else {
-                        process.stdout.write("*" + " ");
-                    }
-                }
-                else {
-                    if (j == ((n - 1) / 2)) {
-                        process.stdout.write("*" + " ");
-                    }
-                    else {
-                        process.stdout.write("#" + " ");
-                    }
-                }
-            }
-        }
-    }
-    else {
-        console.log("nmr3 tidak bisa di eksekusi")
-    }
-}
+const int n = 7;
 
-nmr3(13);
+for (int i = 0; i < n; i++) {
+	for (int j = 0; j < n; j++) {
+		if (i == 0 || i = n-1) {
+			if (j == 0 || j == ((n - 1)/2) || j == n-1) {
+				print "*" + " "
+			}
+			else {
+				print "#" + " "
+			}
+		}
+		else if (i == ((n - 1)/2)) {
+			if (j == ((n - 1)/2)) {
+				print "#" + " "
+			}
+			else {
+				print "*" + " "
+			}
+		}
+		else {
+			if (j == ((n - 1)/2)) {
+				print "*" + " "
+			}
+			else {
+				print "#" + " "
+			}
+		}
+	}
+}
